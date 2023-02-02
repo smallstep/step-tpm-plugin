c3fa147a3256bf967dbd451cdb0f68a2347c4f6f
Herman Slatman <hermanslatman@hotmail.com>
2023-02-02 12:18:10 +0100

    Protect TPM from being concurrently used in `Random`

--- pkg/tpm/random.go
+++ pkg/tpm/random.go
@@ -10,6 +10,11 @@ import (
 
 func (t *TPM) GenerateRandom(ctx context.Context, size uint16) ([]byte, error) {
 
+	if err := t.Open(ctx); err != nil {
+		return nil, fmt.Errorf("failed opening TPM: %w", err)
+	}
+	defer t.Close(ctx, false)
+
 	rwc, err := open.TPM(t.deviceName)
 	if err != nil {
 		return nil, fmt.Errorf("error opening TPM: %w", err)
