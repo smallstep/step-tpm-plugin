6988b1bf553370c371451ba42f1bb95b2747f207
Herman Slatman <hermanslatman@hotmail.com>
2025-06-20 15:55:13 +0200

    Refactor TPM logging to use `logware`

--- cmd/simulator/simulator_enabled.go
+++ cmd/simulator/simulator_enabled.go
@@ -7,7 +7,7 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
+	"log/slog"
 	"net"
 	"os"
 	"os/signal"
@@ -16,6 +16,7 @@ import (
 
 	"github.com/jedib0t/go-pretty/table"
 
+	"github.com/smallstep/panoramix/v5/logware"
 	"go.step.sm/crypto/randutil"
 	"go.step.sm/crypto/tpm"
 	"go.step.sm/crypto/tpm/simulator"
@@ -23,12 +24,24 @@ import (
 	"github.com/smallstep/step-tpm-plugin/internal/flag"
 )
 
+const (
+	loggerName = "tpm"
+)
+
 func runSimulator(ctx context.Context) (err error) {
 	var (
 		socket  = flag.GetString(ctx, flag.FlagSocket)
 		seed    = flag.GetString(ctx, flag.FlagSeed)
 		verbose = flag.GetBool(ctx, flag.FlagVerbose)
+		logger  *slog.Logger
 	)
+
+	loggerOptions := []logware.Option{logware.WithName(loggerName)}
+	if verbose {
+		loggerOptions = append(loggerOptions, logware.WithLevel(slog.LevelDebug))
+	}
+
+	logger = logware.Logger(loggerOptions...)
 
 	if seed == "" {
 		if seed, err = randutil.Hex(16); err != nil {
@@ -110,14 +123,14 @@ func runSimulator(ctx context.Context) (err error) {
 	})
 	t1.Render()
 
-	logF(true, "TPM simulator available at %q\n", socket)
+	logger.InfoContext(ctx, "TPM simulator available", slog.String("socket", socket))
 
 	// register shutdown signals and perform cleanup
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		logF(true, "stopping TPM simulator at %q\n", socket)
+		logger.InfoContext(ctx, "stopping TPM simulator ...", slog.String("socket", socket))
 		os.Remove(socket)
 		os.Exit(0)
 	}()
@@ -130,9 +143,9 @@ func runSimulator(ctx context.Context) (err error) {
 
 		// check if connection was established successfully
 		if err != nil {
-			logF(true, "connection error: %v\n", err)
+			logger.ErrorContext(ctx, "connection error", logware.Error(err))
 		} else {
-			logF(verbose, "connection established: %v\n", conn)
+			logger.DebugContext(ctx, "connection established", slog.String("addr", conn.RemoteAddr().String()))
 		}
 
 		// handle the connection in a separate goroutine.
@@ -143,32 +156,32 @@ func runSimulator(ctx context.Context) (err error) {
 			buf := make([]byte, 4096)
 			n, err := conn.Read(buf)
 			if err != nil {
-				logF(true, "failed reading from socket: %v", err)
+				logger.ErrorContext(ctx, "failed reading fom socket", logware.Error(err))
 			}
 
-			logF(verbose, "read from socket: %v", buf[:n])
+			logger.DebugContext(ctx, "read from socket", slog.Any("bytes", buf[:n]))
 
 			nw, err := s.Write(buf[:n])
 			if err != nil {
-				logF(true, "failed writing to TPM: %v", err)
+				logger.ErrorContext(ctx, "failed writing to TPM", logware.Error(err))
 			}
 
-			logF(verbose, "written to TPM: %v", nw)
+			logger.DebugContext(ctx, "written to TPM", slog.Any("bytes", nw))
 
 			newBuf := make([]byte, 4096)
 			nr, err := s.Read(newBuf)
 			if err != nil {
-				logF(true, "failed reading from TPM: %v", err)
+				logger.ErrorContext(ctx, "failed reading from TPM", logware.Error(err))
 			}
 
-			logF(verbose, "read from TPM: %v", nr)
+			logger.DebugContext(ctx, "read from TPM", slog.Any("bytes", nr))
 
 			_, err = conn.Write(newBuf[:nr])
 			if err != nil {
-				logF(true, "failed writing to socket: %v", err)
+				logger.ErrorContext(ctx, "failed writing to socket", logware.Error(err))
 			}
 
-			logF(verbose, "written to socket: %v", newBuf[:nr])
+			logger.DebugContext(ctx, "written to socket", slog.Any("bytes", newBuf[:nr]))
 
 			// TODO(hs): log at least something about the interaction? The simulator is now very quiet after starting
 		}(conn)
@@ -184,10 +197,4 @@ func getTPMSimulatorSocketPath() (sockAddr string, err error) {
 		}
 	}
 	return "", errors.New("could not automatically determine TPM simulator socket path")
-}
-
-func logF(shouldPrint bool, format string, v ...any) {
-	if shouldPrint {
-		log.Printf(format, v...)
-	}
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.24.0
 toolchain go1.24.1
 
 require (
+	github.com/charmbracelet/colorprofile v0.3.0
 	github.com/charmbracelet/fang v0.1.0
 	github.com/jedib0t/go-pretty v4.3.0+incompatible
 	github.com/smallstep/cli-utils v0.12.2
+	github.com/smallstep/panoramix/v5 v5.0.0-beta1
 	github.com/spf13/cobra v1.10.1
 	github.com/spf13/pflag v1.0.10
 	go.step.sm/crypto v0.73.0
@@ -16,7 +18,6 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/asaskevich/govalidator v0.0.0-20200907205600-7a23bdc65eef // indirect
-	github.com/charmbracelet/colorprofile v0.3.0 // indirect
 	github.com/charmbracelet/lipgloss/v2 v2.0.0-beta.1 // indirect
 	github.com/charmbracelet/x/ansi v0.8.0 // indirect
 	github.com/charmbracelet/x/cellbuf v0.0.13 // indirect
@@ -24,6 +25,8 @@ require (
 	github.com/charmbracelet/x/term v0.2.1 // indirect
 	github.com/chzyer/readline v1.5.1 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.7 // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-openapi/errors v0.20.2 // indirect
 	github.com/go-openapi/strfmt v0.21.3 // indirect
 	github.com/google/btree v1.1.2 // indirect
@@ -32,8 +35,11 @@ require (
 	github.com/google/go-tpm-tools v0.4.6 // indirect
 	github.com/google/go-tspi v0.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/lmittmann/tint v1.1.1 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/manifoldco/promptui v0.9.0 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/mitchellh/mapstructure v1.3.3 // indirect
 	github.com/muesli/cancelreader v0.2.2 // indirect
@@ -46,12 +52,21 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/samber/lo v1.50.0 // indirect
+	github.com/samber/slog-multi v1.4.0 // indirect
 	github.com/schollz/jsonstore v1.1.0 // indirect
 	github.com/smallstep/go-attestation v0.4.4-0.20241119153605-2306d5b464ca // indirect
 	github.com/urfave/cli v1.22.17 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	go.mongodb.org/mongo-driver v1.11.1 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/bridges/otelslog v0.11.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/log v0.12.2 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
 	golang.org/x/crypto v0.43.0 // indirect
 	golang.org/x/sys v0.37.0 // indirect
 	golang.org/x/text v0.30.0 // indirect
+	google.golang.org/grpc v1.76.0 // indirect
 )
